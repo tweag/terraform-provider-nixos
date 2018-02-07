36af1d43179587edc29b8a576480790a501a5daa
Graham Christensen <graham@grahamc.com>
2018-02-07 11:55:07 -0500

    Consolidate the stage two in to the setup, since apparently it works all in one.

--- proof-of-concept/setup.tf
+++ proof-of-concept/setup.tf
@@ -39,3 +39,35 @@ resource "hcloud_server" "web" {
     command = "./resource-nixos-node/setup-nixops.sh  '${hcloud_server.web.name}' '${hcloud_server.web.ipv4_address}' '${var.nixops_root}'"
   }
 }
+
+# This is the second half of the nixos resource,
+# for more, look at ./resource-nixos-node/README
+
+data "local_file" "nixops_web_hardware_config_data" {
+  filename = "./${var.nixops_root}/terraform/.cache/${hcloud_server.web.name}/hardware-configuration.nix"
+}
+
+resource "local_file" "nixops-web-hardware-configuration" {
+  filename = "${path.module}/${var.nixops_root}/terraform/${hcloud_server.web.name}/hardware-configuration.nix"
+  content = <<CONTENT
+    ${data.local_file.nixops_web_hardware_config_data.content}
+  CONTENT
+}
+
+resource "local_file" "nixops-web-default-nix" {
+  filename = "${path.module}/${var.nixops_root}/terraform/${hcloud_server.web.name}/default.nix"
+  content = <<CONTENT
+    { imports = [ ./hardware-configuration.nix ./terraform.nix ];
+      deployment.targetHost = "${hcloud_server.web.ipv4_address}";
+    }
+  CONTENT
+}
+
+resource "local_file" "nixops-web-terraform-nix" {
+  filename = "${path.module}/${var.nixops_root}/terraform/${hcloud_server.web.name}/terraform.nix"
+  content = <<CONTENT
+    {
+      roles.webnode.enable = true;
+    }
+  CONTENT
+}
